840599fb6e18d6f885b8f3f742f975871d9be9a8
bryankruman <bryankruman@gmail.com>
2025-10-20 20:57:06 -0400

    Add popup to open settings page on icon click

---
+++ popup.js
@@ -0,0 +1,21 @@
+﻿(function(){
+  async function openOptions() {
+    try {
+      if (chrome && chrome.runtime && chrome.runtime.openOptionsPage) {
+        await chrome.runtime.openOptionsPage();
+      } else {
+        chrome.tabs.create({ url: chrome.runtime.getURL('options.html') });
+      }
+    } catch (e) {
+      try {
+        chrome.tabs.create({ url: chrome.runtime.getURL('options.html') });
+      } catch (_) {}
+    } finally {
+      // Close the popup regardless of outcome
+      window.close();
+    }
+  }
+
+  // Trigger immediately on load
+  openOptions();
+})();
